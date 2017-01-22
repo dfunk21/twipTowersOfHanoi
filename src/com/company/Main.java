091c7c331d6a08e5f45063b1892e9e689ae3ad0a
dawsonfunk <dawsonfunk@gmail.com>
2017-01-22 15:14:55 -0500

    Finished Towers of Hanoi project

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,21 @@
 package com.company;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to the Towers of Hanoi simulator! Enter the number of\ndisks" +
-                "you would like to play with (non-inclusive of 64). I will print out the\ninstructions for you!");
-        
+        Scanner sb = new Scanner(System.in);
+        System.out.println("Welcome to the Towers of Hanoi simulator!\nEnter the number of disks" +
+                " you would like to play with (up to and non-inclusive of 64).\nI will print out the instructions for you!\n" +
+                "WARNING: If an extremely high number of discs is inputted, program will not run sufficiently!!!!!");
+        int user = sb.nextInt();
+        TowersOfHanoi toh = new TowersOfHanoi();
+
+        System.out.println("Instructions:");
+        toh.solve(user, "1", "2", "3");
+
 
     }
 }
--- src/com/company/TowersOfHanoi.java
+++ src/com/company/TowersOfHanoi.java
@@ -4,4 +4,15 @@ package com.company;
  * Created by df377 on 1/17/17.
  */
 public class TowersOfHanoi {
+
+    public void solve (int y, String begin, String middle, String finish) {
+        if (y == 1) {
+            System.out.println("Move one disc from tower " + begin + " to tower " + finish);
+        }
+        else {
+            solve(y -1, begin, finish, middle);
+            System.out.println("Move one disc from tower " + begin + " to tower " + finish);
+            solve(y - 1, middle, begin, finish);
+        }
+    }
 }
